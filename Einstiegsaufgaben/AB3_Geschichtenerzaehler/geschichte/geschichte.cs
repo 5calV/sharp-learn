924bcaf80e483ee66886afb2b943184e279ae734
5calV <110572069+5calV@users.noreply.github.com>
2023-12-05 13:34:24 +0100

    edited geschichte

--- Einstiegsaufgaben/AB3_Geschichtenerzaehler/geschichte/geschichte.cs
+++ Einstiegsaufgaben/AB3_Geschichtenerzaehler/geschichte/geschichte.cs
@@ -1,2 +1,65 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Fuck youn Pull");
+﻿
+class geschichte
+{
+    static void Main(string[] args)
+    {
+
+        string lebewesen, farbe, teil, verb, name;
+
+        Console.WriteLine("Geschichtenerzähler\n");
+
+        Console.Write("Nenne eine Farbe:       ");
+        farbe = Console.ReadLine();
+
+        Console.Write("Nenne ein Lebewesen:    ");
+        lebewesen = Console.ReadLine();
+
+        Console.Write("Nenne ein Verb:         ");
+        verb = Console.ReadLine();
+
+        Console.Write("Nenne einen Körperteil: ");
+        teil = Console.ReadLine();
+
+        Console.Write("Nenne einen Namen:      ");
+        name = Console.ReadLine();
+
+        Console.WriteLine("\n\nEs war einmal ein {0}.", lebewesen);
+        Console.WriteLine("Er hatte einen {0}en {1}.", farbe, teil);
+        Console.WriteLine("Auf dem Weg zur Schule sah er {0}.", name);
+        Console.WriteLine("Da musste der {0} vor Freude so {1},",lebewesen, verb);
+        Console.WriteLine("dass sein {0} wackelte und {1} ", teil, name ); 
+        Console.WriteLine("auch ganz {0} wurde.", farbe);
+
+        Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 Repository for ITS231 to collaborate on projects for programming class.
 
 ## Pull Requests
-Before creating a pull request PLEASE delete /bin and /obj folders in the project you worked on.
+Before creating a pull request PLEASE delete /bin and /obj folders aswell as the sln file in the project you worked on.
 
 ## Help! It does not compile?!
 Make sure that the .NET version you have installed is the same as the one specified in the .csproj file.
