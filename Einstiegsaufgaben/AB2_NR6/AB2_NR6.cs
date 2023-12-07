bbdb55e8532a9acf9f7c2a73107fdf8c8663bff4
5calV <110572069+5calV@users.noreply.github.com>
2023-12-07 14:28:35 +0100

    added AB2_NR6

---
+++ Einstiegsaufgaben/AB2_NR6/AB2_NR6.cs
@@ -0,0 +1,25 @@
+﻿
+class AB2
+{
+    public static void Main(string[] args)
+    {
+        /*
+        6.	Erstellen Sie ein Programm, das zwei Zahlen einliest. 
+        Diese zwei Zahlen sollen dividiert werden. Das Ergebnis 
+        als ganze Zahl und der Restwert werden einzeln ausgegeben.
+
+        Geben Sie den Dividenden ein: 3
+        Geben Sie den Divisor ein (keine 0): 2
+        Ergebnis: 1
+        Der Rest der Division ist 1.
+        */
+
+
+
+
+
+
+
+
+    }
+}
