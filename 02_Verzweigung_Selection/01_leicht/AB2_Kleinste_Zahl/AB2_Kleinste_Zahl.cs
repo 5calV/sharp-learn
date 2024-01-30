befd5e4f49c90a883bfe949d68ab2cae761f5c19
5calV <110572069+5calV@users.noreply.github.com>
2024-01-30 09:02:17 +0100

    added AB2_Kleinste_Zahl

---
+++ 02_Verzweigung_Selection/01_leicht/AB2_Kleinste_Zahl/AB2_Kleinste_Zahl.cs
@@ -0,0 +1,31 @@
+﻿
+class Kleinste_Zahl {
+
+    public static void Main(string[] args) {
+
+        Console.WriteLine("Zahlenvergleich");
+
+        int zahl1, zahl2;
+
+        Console.WriteLine("Bitte erste Zahl eingeben:");
+        zahl1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Bitte zweite Zahl eingeben:");
+        zahl2 = Convert.ToInt32(Console.ReadLine());
+
+        if (zahl1 == zahl2) {
+            Console.WriteLine(zahl1 + " und " + zahl2 + " sind gleich groß.");
+        }
+        else if (zahl1 < zahl2) {
+            Console.WriteLine(zahl1 + " ist kleiner als " + zahl2);
+        }
+        else {
+            Console.WriteLine(zahl1 + " ist größer als " + zahl2);
+        }
+
+
+
+
+
+    }
+}
